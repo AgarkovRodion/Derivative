dd1d889a4c1ec30ef6db8a97722a548f9108b806
unknown <admin@508EC2.att.edu.ru>
2022-12-02 17:35:04 +0300

    Initial commit

---
+++ src/Derivative.java
@@ -0,0 +1,47 @@
+
+
+public class Derivative {
+   String function;
+  boolean checkString() {
+      return false;
+  }
+
+  void checkForFunction() {
+      checkString();
+      //
+  }
+
+  void countDerivative() {
+      checkForFunction();
+
+      if() {
+
+         countSinFunc();
+      }
+      else if() {
+          countCosFunc();
+      }
+
+      //расчёт производной
+  }
+
+  void countSinFunc() {
+      if (function.equals("sin")){
+          System.out.println("cos");
+      }
+      //расчёт производной синуса
+  }
+
+    void countCosFunc() {
+      if (function.equals("cos"))
+
+    }
+
+    void countTgFunc() {
+
+    }
+
+    void countCtgFunc() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите функцию");
+        Derivative derivative = new Derivative();
+
+        derivative.function = scanner.next();
+
+    }
+}
